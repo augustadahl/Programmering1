ac42e375f4a0c0fca67e98357160a8caf642a73a
Augustadahl <august.adahl@outlook.com>
2018-09-05 16:17:26 +0200

    Quadrant sellection

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,4 +1,36 @@
+import java.util.Scanner;
 
 public class Helloworld {
 
-}
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int x = input.nextInt();
+		
+		int y = input.nextInt();
+	
+		input.close();
+	
+		if(x>0 && y>0) {
+			System.out.println(1);
+		}
+		
+		if(x>0 && y<0) {
+			System.out.println(4);
+		}
+		
+		if(x<0 && y>0) {
+			System.out.println(2);
+		}
+		
+		if(x<0 && y<0) {
+			System.out.println(3);
+		}
+		
+		
+	} // End Main 
+	
+	
+} // End Helloworld
