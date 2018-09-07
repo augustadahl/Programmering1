3c9f165e9c16fb76b80313bf272b2812ff4c1b28
Augustadahl <august.adahl@outlook.com>
2018-09-07 15:12:38 +0200

    Frågar efter namn

--- src/Läxa36.java
+++ src/Läxa36.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
 
 public class Läxa36 {
 
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Vad heter du");
+		
+		String name = input.nextLine();
+		
+		System.out.println(name);
+		
+	}
+	
+	
+	
+	
+	
 }
