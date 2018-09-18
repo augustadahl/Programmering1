1e78c88a5d56269303c2ec1867ea69d9059549b7
Augustadahl <august.adahl@outlook.com>
2018-09-18 14:56:19 +0200

    Testar vilket värde som är störst

--- src/Läxa_v37.java
+++ src/Läxa_v37.java
@@ -24,12 +24,43 @@ public class Läxa_v37 {
 		int summa = tal1+tal2+tal3+tal4+tal5+tal6+tal7+tal8+tal9+tal10; //räknar ut medelvärdet
 		double medel = summa / 10.0;
 		
-		int stor =
+		int stor = tal1; //1
+		
+		if (stor <= tal2) { //2
+			stor = tal2;
+		}
+		if (stor <= tal3) { //3
+			stor = tal3;
+		}
+		if (stor <= tal4) { //4
+			stor = tal4;
+		}
+		if (stor <= tal5) { //5
+			stor = tal5;
+		}
+		if (stor <= tal6) { //6
+			stor = tal6;
+		}
+		if (stor <= tal7) { //7
+			stor = tal7;
+		}
+		if (stor <= tal8) { //8
+			stor = tal8;
+		}
+		if (stor <= tal9) { //9
+			stor = tal9;
+		}
+		if (stor <= tal10) { //10
+			stor = tal10;
+		}
+		
+		
 		
 		// om tal2 >= största tal så är tal2 störst just nu annars testa nästa tal
-		System.out.println(tal1);
+
 		System.out.println(summa);
-		System.out.println(medel);
+		System.out.println("medel: " + medel);
+		System.out.println("största talet är: " + stor);
 		
 	}
 	
