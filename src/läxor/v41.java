068250483cf3ccee6f0b00ac09d20a5829a68927
augustadahl <august.adahl@outlook.com>
2018-10-16 15:20:31 +0200

    Läxa klar

--- src/läxor/v41.java
+++ src/läxor/v41.java
@@ -12,9 +12,9 @@ public class v41 {
 		
 		//System.out.println(reverse(sc.nextLine()));
 		
-		System.out.println(count(sc.nextLine(), sc.next().charAt(0)));
+		//System.out.println(count(sc.nextLine(), sc.next().charAt(0)));
 		
-		//System.out.println(sjorovare(sc.nextLine()));
+		System.out.println(sjorovare(sc.nextLine()));
 		
 	}
 	
@@ -54,55 +54,36 @@ public class v41 {
 		
 		String sjorovare = "";
 		
-		String vokaler = "aouåeiyäö";
-		
 		String konsonanter = "bcdfghjklmnpqrstvwz";
 		
-		String StoraVokaler = "AOUÅEIYÄÖ";
-		
-		String StoraKonsonanter = "BCDFGHJKLMNPQRSTVWZ";
+		boolean text = false;
+
 		
 		for(int i = 0; i < str.length(); i++) {
 			
-			for(int a = 0; a < vokaler.length(); a++) {
-				
-				if(str.charAt(i) == vokaler.charAt(a)) {
-					sjorovare = sjorovare + str.charAt(i);
-				}	
-			}
+			text = false;
 			
-			
-			for(int c = 0; c < StoraVokaler.length(); c++) {
+			for(int a = 0; a < konsonanter.length(); a++) {
 				
-				if(str.charAt(i) == StoraVokaler.charAt(c)) {
-					sjorovare = sjorovare + str.charAt(i);
-				}	
-			}
-			
-			for(int b = 0; b < konsonanter.length(); b++) {
-				
-				if(str.charAt(i) == konsonanter.charAt(b)) {
-					sjorovare = sjorovare + str.charAt(i) + 'o' + str.charAt(i);
-				}				
-			}
-			
-			for(int d = 0; d < StoraKonsonanter.length(); d++) {
-				
-				if(str.charAt(i) == StoraKonsonanter.charAt(d)) {
+				if(str.toLowerCase().charAt(i) == konsonanter.charAt(a)) {
 					sjorovare = sjorovare + str.charAt(i) + 'o' + str.toLowerCase().charAt(i);
-				}				
+					text = true;
+				}
+				
 			}
 			
 			if(str.charAt(i) == 'x') {
 				sjorovare = sjorovare + "koksos";
+				text = true;
 			}
 			
 			if(str.charAt(i) == 'X') {
 				sjorovare = sjorovare + "Koksos";
+				text = true;
 			}
 			
-			if(str.charAt(i) == ' ') {
-				sjorovare = sjorovare + ' ';
+			if(text != true) {
+				sjorovare = sjorovare + str.charAt(i);
 			}
 			
 		}
