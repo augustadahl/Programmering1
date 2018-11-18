21db911ca28b654f877197cdd5481d8b01dd6bd1
augustadahl <august.adahl@outlook.com>
2018-11-18 20:18:43 +0100

    Lade till en funktion

--- src/saker/bubble_sort.java
+++ src/saker/bubble_sort.java
@@ -10,9 +10,9 @@ public class bubble_sort {
 		
 		System.out.println("Unsorted :");
 
-		int[] intArr = new int[random.nextInt(50 - 20) + 20];
+		int[] intArr = new int[random.nextInt(50 - 20) + 20]; //Skapar en int array som är mellan 50 och 20 stor
 
-		for (int i = 0; i < intArr.length; i++) {
+		for (int i = 0; i < intArr.length; i++) { //Ger platserna i arrayen en int mellan 0 och 100
 
 			intArr[i] = random.nextInt(100 - 0) + 0;
 			System.out.print(intArr[i] + " ");
@@ -20,12 +20,13 @@ public class bubble_sort {
 		
 		System.out.println();
 
+		int compares = 0;
 
 		int temp;
 		
 		int unsorted = intArr.length - 1;
 
-		while (unsorted > 0) {
+		while (unsorted > 0) { //Använder bubblesort för att sortera dessa värden efter storleksordning
 			unsorted = intArr.length - 1;
 			for (int i = 0; i < intArr.length - 1; i++) {
 				if (intArr[i] > intArr[i + 1]) {
@@ -36,6 +37,7 @@ public class bubble_sort {
 				else {
 					unsorted--;
 				}
+				compares++;
 			}			
 		}
 		
@@ -44,6 +46,8 @@ public class bubble_sort {
 		for (int i = 0; i < intArr.length; i++) {
 			System.out.print(intArr[i] + " ");
 		}
+		System.out.println();
+		System.out.println("Compares = " + compares);
 
 		
 		
