9fa1f7cf36b997b757b168d4e3b04ca61c7f04b1
augustadahl <august.adahl@outlook.com>
2018-12-09 12:03:50 +0100

    Klar med train passengers

--- src/Kattis/train_passagers.java
+++ src/Kattis/train_passagers.java
@@ -8,8 +8,50 @@ public class train_passagers {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		int capacity = sc.nextInt();
 		
+		int stations = sc.nextInt();
 		
+		int passengers = 0;
+		
+		int waiting = 0;
+		
+		boolean possible = true;
+		
+		for (int i = 0; i < stations; i++) {
+			
+			passengers -= sc.nextInt();
+			
+			if (passengers < 0) {
+				possible = false;
+			}
+			
+			
+			passengers += sc.nextInt();
+			
+			if (passengers > capacity) {
+				possible = false;
+			}
+			
+			
+			waiting = sc.nextInt();
+			
+			if (waiting > 0 && passengers < capacity) {
+				possible = false;
+			}
+			
+		}
+		
+		if (passengers > 0 || waiting > 0) {
+			possible = false;
+		}
+		
+		if (possible == true) {
+			System.out.println("possible");
+		}
+		else {
+			System.out.println("impossible");
+		}
 		
 		
 		
