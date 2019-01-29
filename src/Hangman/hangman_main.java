19e0ec2c4d1192512c50d4f348220c8d3e42dc64
augustadahl <august.adahl@outlook.com>
2019-01-29 22:10:39 +0100

    Klar med hänga gubbe

--- src/Hangman/hangman_main.java
+++ src/Hangman/hangman_main.java
@@ -1,15 +1,9 @@
 package Hangman;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
-//Läsa in en mening
-//Skriva ut _____ __ fär ordet/meningen
-//Läsa in bokstäver som gissas
-//Skriva ut om det är korrekt eller inte
-//Skriva ut antalet LIMBS som är kvar
-//Kontrollera om man vunnit eller ej
+//Skriv in hemligt ord och gissa därefter dig fram till rätt ord en bokstav i taget
 
 
 public class hangman_main {
@@ -39,11 +33,86 @@ public class hangman_main {
 		
 		ArrayList<Character> taken = new ArrayList<Character>();
 		
-		status(SecretHidden, limbs, taken);
-		System.out.println("None");
+		boolean lost = true;
 		
-		while (true) {
+		status(SecretHidden, limbs, taken, lost);
+		
+		boolean running = true;
+		
+		
+		//_________________________________________________________________________
+		
+		
+		while (running) {
 			
+			lost = false;
+			
+			char letter = sc.next().charAt(0);
+			
+			if (validater(taken, letter)) {
+				boolean wrong = true;
+				
+				char[] temp = new char[SecretHidden.length];
+				
+				for (int i = 0; i < SecretHidden.length; i++) {
+					temp[i] = SecretHidden[i];
+				}
+				
+				SecretHidden = updater(SecretHidden, SecretVisible, letter); 
+				
+				taken.add(letter);
+				
+				for (int i = 0; i < temp.length; i++) {
+					if (temp[i] != SecretHidden[i]) {
+						wrong = false;
+					}
+				}
+				
+				if (wrong) {
+					limbs--;
+					if (limbs <= 6) {
+						lost = true;
+					}
+				}
+				
+				if (taken.size() == 4) {
+					boolean nuke = true;
+					
+					String easteregg = "nuke";
+					
+					for (int i = 0; i < taken.size(); i++) {
+						if (easteregg.charAt(i) != Character.toLowerCase(taken.get(i))) {
+							nuke = false;
+						}
+					} 
+					
+					if (nuke) {
+						limbs += 8;
+						lost = true;
+					}
+					
+				}
+				
+				status(SecretHidden, limbs, taken, lost);
+			}
+			
+			if (limbs == 0) {
+				running = false;
+				System.out.println("Game Over");
+			}
+			
+			boolean win = true;
+			
+			for (int i = 0; i < SecretVisible.length; i++) {
+				if (SecretVisible[i] != SecretHidden[i]) {
+					win = false;
+				}
+			}
+			
+			if (win) {
+				System.out.println("YOU WIN!!!");
+				running = false;
+			}
 			
 			
 		}
@@ -52,7 +121,41 @@ public class hangman_main {
 	
 	
 	
-	public static void status(char[] SecretHidden, int limbs, ArrayList<Character> taken) {
+	
+	public static boolean validater(ArrayList<Character> taken, char letter) {
+		
+		boolean yes = true;
+		
+		for (int i = 0; i < taken.size(); i++) {
+			if (taken.get(i) == letter || letter == ' ') {
+				yes = false;
+			}
+		}
+		
+		if (!yes) {
+			System.out.println("WRONG");
+			return false;
+		}
+		else {
+			return true;
+		}
+		
+	}
+
+	
+	public static char[] updater(char[] SecretHidden, char[] SecretVisible, char letter) {
+		
+		for (int i = 0; i < SecretVisible.length; i++) {
+			if (Character.toLowerCase(letter) == Character.toLowerCase(SecretVisible[i])) {
+				SecretHidden[i] = SecretVisible[i];
+			}
+		}
+		
+		return SecretHidden;
+		
+	}
+	
+	public static void status(char[] SecretHidden, int limbs, ArrayList<Character> taken, boolean lost) {
 		
 		System.out.println("Game Status:");
 		
@@ -66,8 +169,7 @@ public class hangman_main {
 		
 		System.out.println();
 		
-		System.out.println("Limbs remaining: " + limbs);
-		
+
 		System.out.println("Taken Characters:");
 		
 		System.out.print("|");
@@ -78,7 +180,38 @@ public class hangman_main {
 		
 		System.out.println();
 		
+		System.out.println("Limbs remaining: " + limbs);
+		
+		
+		
+		if (lost) {
+			if (limbs > 6) {
+				System.out.println("*Chernobly is hell of a place*");
+			}
+			if (limbs == 6) {
+				System.out.println("*you're one healthy bastard*");
+			}
+			if (limbs == 5) {
+				System.out.println("*You just lost a leg*");
+			}
+			if (limbs == 4) {
+				System.out.println("*Aw man not even a cane can help you now*");
+			}
+			if (limbs == 3) {
+				System.out.println("*Lets hope you still have your good arm*");
+			}
+			if (limbs == 2) {
+				System.out.println("*Keep yo head cool*");
+			}
+			if (limbs == 1) {
+				System.out.println("*PROTECT THE COCK AT ALL COSTS*");
+			} 
+		}
+		
+		
 	}
+	
+	
 	
 	
 }
